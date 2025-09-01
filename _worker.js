d08db1a24944652f8e6757b01a9e8df0ac0a7bd5
budeg <ndablegbudeg@gmail.com>
2025-09-01 15:17:21 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "emboehrareti.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "yachya44.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "vmess"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
